71fcacf0cec49a3a8c794dfac3182dbb381baa55
Katherine <katherine.szelag@mail.utoronto.ca>
2020-01-09 19:30:01 +0000

    get my reservations query

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -75,12 +75,6 @@ const addUser =  function(user) {
 exports.addUser = addUser;
 
 
-// const userId = Object.keys(users).length + 1;
-// user.id = userId;
-// users[userId] = user;
-// return Promise.resolve(user);
-
-
 
 
 
@@ -91,17 +85,24 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
-const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+const getAllReservations = function(guest_id) {
+  return pool.query(
+    `SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+    FROM properties
+    JOIN reservations ON properties.id = reservations.property_id
+    JOIN property_reviews ON properties.id = property_reviews.property_id
+    WHERE reservations.guest_id = $1 
+    GROUP BY properties.id, reservations.id
+    ORDER BY reservations.start_date
+    LIMIT 10;`, [guest_id])
+    .then(function(res) {
+      return res.rows;
+    })
 }
 exports.getAllReservations = getAllReservations;
 
 
-
-
-
-
-
+//AND reservations.end_date < now()::date
 
 /// Properties
 
@@ -112,7 +113,7 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 
-const getAllProperties = function(options, limit = 10) {
+const getAllProperties = function(options, limit) {
   return pool.query(`
   SELECT * FROM properties
   LIMIT $1
